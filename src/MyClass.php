13f143ad7c865ced1c37b5b567f2c733e072e79c
Roman <masorinwork@gmail.com>
2019-07-13 14:39:44 +0300

    setup scrutinizer

--- src/MyClass.php
+++ src/MyClass.php
@@ -1,9 +1,17 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App;
 
 class MyClass
 {
+    /**
+     * @param int $one
+     * @param int $two
+     *
+     * @return int
+     */
     public function sum(int $one, int $two): int
     {
         if (100 === $one) {
