481ee196a3f076e4510ce088b53d2c984566d09f
Roman <masorinwork@gmail.com>
2019-07-22 13:05:37 +0300

    fixed CacheTest

--- src/Cache.php
+++ src/Cache.php
@@ -4,13 +4,14 @@ declare(strict_types=1);
 
 namespace App;
 
-use App\Exception\EmptyKeyException;
 use App\Exception\EmptyPoolException;
 use App\Exception\KeyNotFoundException;
 use App\Exception\OutdatedCacheException;
+use App\Traits\EmptyKeyExceptionTrait;
 
 class Cache implements CacheLayerInterface
 {
+    use EmptyKeyExceptionTrait;
     /**
      * @var CacheLayerInterface[]
      */
@@ -21,10 +22,7 @@ class Cache implements CacheLayerInterface
      */
     public function get(string $key)
     {
-        if ('' === trim($key)) {
-            throw new EmptyKeyException();
-        }
-
+        $this->checkIsEmptyKeyException($key);
         $this->checkIsNotEmptyPool();
 
         $outdatedKey = false;
@@ -45,21 +43,11 @@ class Cache implements CacheLayerInterface
      */
     public function set(string $key, $value, $ttl = 3600): void
     {
-        if ('' === trim($key)) {
-            throw new EmptyKeyException();
-        }
-
+        $this->checkIsEmptyKeyException($key);
         $this->checkIsNotEmptyPool();
 
         foreach ($this->layers as $layer) {
             $layer->set($key, $value, $ttl);
-        }
-    }
-
-    private function checkIsNotEmptyPool()
-    {
-        if (empty($this->layers)) {
-            throw new EmptyPoolException();
         }
     }
 
@@ -69,5 +57,12 @@ class Cache implements CacheLayerInterface
     public function addLayer(CacheInterface $layer): void
     {
         $this->layers[] = $layer;
+    }
+
+    private function checkIsNotEmptyPool()
+    {
+        if (empty($this->layers)) {
+            throw new EmptyPoolException();
+        }
     }
 }
